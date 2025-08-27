223569f1dec09f743ed2074b706c4c69a17fb250
dharshanb0025 <dharshanb0025@gmail.com>
2025-08-27 14:38:29 +0530

    updated screens

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { COLORS } from '@/constants/colors.js';
-const SafeScreen = ({children}) => {
-    const insets=useSafeAreaInsets();
-  return (
-    <View style={{paddingTop: insets.top, paddingBottom: insets.bottom, flex: 1,backgroundColor: COLORS.background}}>
-       {children}
-    </View>
-  )
-}
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default SafeScreen
+import { COLORS } from "@/constants/colors";
+
+const SafeScreen = ({ children }) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={{ paddingTop: insets.top, flex: 1, backgroundColor: COLORS.background }}>
+      {children}
+    </View>
+  );
+};
+
+export default SafeScreen;
